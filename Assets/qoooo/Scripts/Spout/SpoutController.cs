dad2a59bc9207fe749566e27c5009b337a2d0dbb
koya-kimura <2ktmnri8@gmail.com>
2026-09-04 23:20:04 +0900

    spout送れた

---
+++ Assets/qoooo/Scripts/Spout/SpoutController.cs
@@ -0,0 +1,21 @@
+﻿using Klak.Spout;
+using UnityEngine;
+
+namespace qoooo.Scripts.Spout
+{
+    public class SpoutController : MonoBehaviour
+    {
+        [SerializeField] Camera _camera;
+        [SerializeField] SpoutSender _spoutSender;
+        [SerializeField] Vector2Int _resolution;
+
+        private RenderTexture _output;
+
+        private void Start()
+        {
+            _output = new RenderTexture(_resolution.x, _resolution.y, 0);
+            _camera.targetTexture = _output;
+            _spoutSender.sourceTexture = _output;
+        }
+    }
+}
